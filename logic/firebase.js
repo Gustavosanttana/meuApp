0a188aeab4731dae8dc0b8cb44121334b9bd1d6c
Gustavo santana <105515366+Gustavosanttana@users.noreply.github.com>
2022-08-27 10:35:50 -0300

    upload

---
+++ logic/firebase.js
@@ -0,0 +1,14 @@
+// insira suas credenciais aqui
+ const firebaseConfig = {
+    apiKey: "AIzaSyDqs9gtn8BHut1l1G-DXYhQeg597l2ElgI",
+    authDomain: "to-do-list-28b1c.firebaseapp.com",
+    databaseURL: "https://to-do-list-28b1c-default-rtdb.firebaseio.com",
+    projectId: "to-do-list-28b1c",
+    storageBucket: "to-do-list-28b1c.appspot.com",
+    messagingSenderId: "335781366444",
+    appId: "1:335781366444:web:f50b90ff46d72bdde5ad09"
+  
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
